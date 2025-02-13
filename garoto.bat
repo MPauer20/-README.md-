9e671676893d3b800b17711309d3f6eca1d5101f
MPauer20 <rmarcosgregorio@gmail.com>
2025-02-13 14:12:46 -0300

    Add files via upload

---
+++ garoto.bat
@@ -0,0 +1,20 @@
+mkdir Mario
+pause
+rmdir Mario
+mkdir Luidy
+pause
+cd Luidy
+echo. >mega.txt
+copy mega.txt Luidy
+pause
+cd ..
+mkdir roda
+move Luidy\mega.txt roda\rodao.txt
+pause
+cd roda 
+rename rodao.txt rodinha.txt
+pause
+del rodinha.txt
+echo ola rodinha, como vc esta. >locabreu.txt
+type locabreu.txt 
+pause
